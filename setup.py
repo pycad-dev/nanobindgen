0348245a0e4697d7e90c89260b4e64d32a81b922
Alex Koen <git@koen.ca>
2024-09-27 11:45:29 -0700

    ci: force non-universal

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ class bdist_wheel(_bdist_wheel):
         # Mark the wheel as non-pure (platform-specific)
         super().finalize_options()
         self.root_is_pure = False
+        self.universal = False
 
     def get_tag(self):
         python, abi, plat = super().get_tag()
