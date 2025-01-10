9da1486b8bf9f50d67a089f768671444b1ea7f8e
Alex Koen <git@koen.ca>
2025-01-10 08:55:50 -0800

    feat: bind enums

--- tests/expected_output.h
+++ tests/expected_output.h
@@ -5,7 +5,7 @@
 void bind_input(nb::module_ &m)
 {
     // Classes
-    nb::class_<Test>(m, "Test")
+    nb::class_<Test>(m, "Test", nb::intrusive_ptr<Test>([](Test* o, PyObject* po) noexcept { o->set_self_py(po); })
         .def(nb::init<>(), "Construct a new Test object")
         .def(nb::init<int &>(), "a"_a, "Construct a new Test object")
         .def("nontrivial_params", &Test::nontrivial_params, "a"_a, "b"_a = nullptr, "c"_a = std::vector<int>(), "Complex parameters\n\nArgs:\n    a: a\n    b: b\n    c: c")
@@ -15,9 +15,10 @@ void bind_input(nb::module_ &m)
         .def("__magico__", nb::overload_cast<double &>(&Test::overload_magic), "a"_a, "An overloaded magic function\n\nArgs:\n    a: First param")
         .def("__magico__", nb::overload_cast<int>(&Test::overload_magic), "b"_a, "An overloaded magic function\n\nArgs:\n    b: Alternate param");
 
-    nb::class_<Test2>(m, "Test2")
-        .def_prop_rw("name", &Test2::get_name, Test2::set_name, "Get the name object\n\nReturns: The name")
-        .def_prop_ro("prop", &Test2::get_prop, "Read-only property");
+    nb::class_<Test2, Test>(m, "Test2")
+        .def_prop_rw("name", &Test2::get_name, &Test2::set_name, "Get the name object\n\nReturns: The name")
+        .def_prop_ro("prop", &Test2::get_prop, "Read-only property")
+        .def("__iter__", &Test2::iter, "Iterate through this list\n\nReturns: iterator", nb::keep_alive<0, 1>());
 
     // Functions
     m.def("free", &free, "a"_a, "b"_a, "A free function\n\nArgs:\n    a: Hello\n    b: Bye\n\nReturns: Fye");
@@ -25,5 +26,8 @@ void bind_input(nb::module_ &m)
     m.def("free_return_simple", &free_return_simple, "A simple return type\n\nReturns: Simple return type");
 
     // Enums
-
+    nb::enum_<TestEnum>(m, "TestEnum")
+        .value("CAR", TestEnum::CAR)
+        .value("TRUCK", TestEnum::TRUCK)
+        .value("BIKE", TestEnum::BIKE);
 };
